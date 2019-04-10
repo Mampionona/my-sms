0d56c4d7c9249dfe17ea7a2c8218e9407106f259
Mampionona <devke03@gmail.com>
2019-04-10 12:18:43 +0300

    adding bootstrap

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+window.Popper = require('popper.js').default;
+window.$ = window.jQuery = require('jquery');
+require('bootstrap');
+
 const token = localStorage.getItem('token');
 if (token) {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
