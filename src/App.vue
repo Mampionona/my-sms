3beffc8ff5a737231e7259f510185702ddbdbe77
Mampionona <devke03@gmail.com>
2019-04-10 15:21:30 +0300

    transitions

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <component :is="layout">
-      <router-view/>
+      <transition name="slide-fade" mode="out-in">
+        <router-view/>
+      </transition>
     </component>
   </div>
 </template>
@@ -43,3 +45,18 @@ export default {
   }
 }
 </script>
+<style lang="scss">
+/* Enter and leave animations can use different */
+/* durations and timing functions.              */
+.slide-fade-enter-active {
+  transition: all .3s ease;
+}
+.slide-fade-leave-active {
+  transition: all .2s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.slide-fade-enter, .slide-fade-leave-to
+/* .slide-fade-leave-active below version 2.1.8 */ {
+  transform: translateX(10px);
+  opacity: 0;
+}
+</style>
