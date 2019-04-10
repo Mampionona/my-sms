094f9c72f0c5aab4db106b3d150423c1d1b18801
Mampionona <devke03@gmail.com>
2019-04-10 09:19:50 +0300

    login & register pages

--- src/components/layouts/Default.vue
+++ src/components/layouts/Default.vue
@@ -2,7 +2,32 @@
   <div class="default-layout">
     <app-header></app-header>
     <div class="main-content">
-      <slot></slot>
+      <div class="header bg-gradient-primary py-7 py-lg-8 pt-lg-9">
+        <div class="container">
+          <div class="header-body text-center mb-7">
+            <div class="row justify-content-center">
+              <div class="col-xl-5 col-lg-6 col-md-8 px-5">
+                <h1 class="text-white">
+                  <slot name="title"></slot>
+                </h1>
+                <p class="text-lead text-white">
+                  <slot name="intro"></slot>
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="separator separator-bottom separator-skew zindex-100">
+          <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+            <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+          </svg>
+        </div>
+      </div>
+      <div class="container mt--8 pb-5">
+        <div class="row justify-content-center">
+          <slot></slot>
+        </div>
+      </div>
     </div>    
     <app-footer></app-footer>
   </div>
