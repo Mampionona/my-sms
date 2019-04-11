d265fb123caab1dba0d5876380763ba69842d0a0
smspartner2019 <devke03@gmail.com>
2019-04-11 16:48:16 +0300

    alert component

---
+++ src/components/Alert.vue
@@ -0,0 +1,32 @@
+<template>
+  <div :class="classObject" role="alert">
+    <span v-if="icon" class="alert-icon"><i :class="icon"></i></span>
+    <span class="alert-text">
+      <slot></slot>
+    </span>
+    <button v-if="dismissible" type="button" class="close" data-dismiss="alert" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    classObject () {
+      const color = `alert-${this.color}`;
+      return {
+        alert: true,
+        fade: true,
+        show: true,
+        [ color ]: this.color,
+        'alert-dismissible': this.dismissible
+      }
+    }
+  },
+  props: {
+    icon: { type: String },
+    dismissible: { type: Boolean },
+    color: { type: String }
+  }
+}
+</script>
