ad2d69cc0458d4d8821379b0aa0d2ca577a8b10f
smspartner2019 <devke03@gmail.com>
2019-04-11 10:46:37 +0300

    method for removing spaces for a given string

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,11 +1,14 @@
-const axios = require('axios');
+window.axios = require('axios');
 window.Popper = require('popper.js').default;
 window.$ = window.jQuery = require('jquery');
 require('bootstrap');
 
 const token = localStorage.getItem('token');
 if (token) {
-  axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+  window.axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 }
 
-window.axios = axios;
+// Remove spaces
+String.prototype.removeSpaces = function() {
+  return this.replace(/\s/g, '').trim();
+};
