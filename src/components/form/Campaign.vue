22d8c4aa9135c4b74d2a0e11b021b02bfef0f4e5
smspartner2019 <devke03@gmail.com>
2019-04-17 16:55:58 +0300

    add campaign's name

--- src/components/form/Campaign.vue
+++ src/components/form/Campaign.vue
@@ -4,24 +4,25 @@
       <div class="row">
         <div class="col-lg-4">
           <div class="card">
+            <div class="card-header"><i class="far fa-comment mr-2"></i> Nom de la campagne</div>
+            <div class="card-body">
+              <input type="text" class="form-control" v-model="name" placeholder="Nom de la campagne">
+            </div>
+          </div>
+          <div class="card">
             <div class="card-header"><i class="fas fa-users mr-2"></i> Destinataires</div>
             <div class="card-body">
               <model-list-select
                 :list="lists" 
                 option-value="id" 
                 option-text="name" 
-                v-model="to"
+                v-model="listId"
                 placeholder="Sélectionner un fichier"
               ></model-list-select>
-              <p v-if="countContacts > 0 && to" class="small mt-4">{{ $tc('redaction.countContacts', countContacts) }}</p>
+              <p v-if="countContacts > 0 && listId" class="small mt-4">{{ $tc('redaction.countContacts', countContacts) }}</p>
             </div>
           </div>
-          <div class="card">
-            <div class="card-header"><i class="fas fa-arrow-right mr-2"></i> Émetteur du message</div>
-            <div class="card-body">
-              <input type="text" class="form-control" v-model="senderName" placeholder="Nom de l'émetteur">
-            </div>
-          </div>
+          
         </div>
         <div class="col-lg-4">
           <div class="card">
@@ -31,18 +32,30 @@
                 <textarea-autosize
                   placeholder="Votre message..."
                   v-model="text"
-                  :min-height="178"
-                  :max-height="300"
+                  :min-height="129"
+                  :max-height="210"
                   class="form-control-plaintext small"
                 ></textarea-autosize>
                 <p class="small text-right">
                   <strong>{{ countSMS }}</strong> SMS - {{ $tc("remainingChars", remainingChars) }} <strong>{{ remainingChars }}</strong>
                 </p>
               </div>
+              <div class="mt-4">
+                <div class="custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" id="save-as-draft" v-model="status">
+                  <label class="custom-control-label" for="save-as-draft">Enregistrer en tant que brouillon</label>
+                </div>
+              </div>
             </div>
           </div>
         </div>
         <div class="col-lg-4">
+          <div class="card">
+            <div class="card-header"><i class="fas fa-arrow-right mr-2"></i> Émetteur du message</div>
+            <div class="card-body">
+              <input type="text" class="form-control" v-model="senderName" placeholder="Nom de l'émetteur">
+            </div>
+          </div>
           <div class="card">
             <div class="card-header"><i class="fas fa-calendar-alt mr-2"></i> Date d'envoi</div>
             <div class="card-body">
@@ -67,14 +80,6 @@
                 @input="dateChange"
                 :disabled-days="date => date.getDay() === 0"
               ></date-picker>
-            </div>
-          </div>
-          <div class="card">
-            <div class="card-body">
-              <div class="custom-control custom-checkbox">
-                <input type="checkbox" class="custom-control-input" id="save-as-draft" v-model="status">
-                <label class="custom-control-label" for="save-as-draft">Enregistrer en tant que brouillon</label>
-              </div>
             </div>
           </div>
         </div>
@@ -112,7 +117,6 @@ export default {
   },
   data () {
     return {
-      to: '',
       text: '',
       senderName:'',
       sendDate: '',
@@ -120,7 +124,7 @@ export default {
       countSMS: 1,
       sendingMode: 'immediate',
       status: false,
-      listId: null,
+      listId: '',
       name: ''
     };
   },
@@ -134,16 +138,9 @@ export default {
       this.countSMS = computeNumberOfSMS(newText);
       this.remainingChars = computeRemainingChars(this.countSMS, newText.length);
     },
-    to (listId) {
+    listId (newListId) {
       const vm = this;
-      this.lists.forEach(list => {
-        if (list.id === listId) {
-          vm.listId = listId;
-          vm.name = list.name;
-        }
-      });
-
-      this.getContactsOfList(listId);
+      this.getContactsOfList(newListId);
     },
     '$route': 'populateCampainFields'
   },
@@ -178,10 +175,9 @@ export default {
 
       this.createOrUpdateCampaign({ action, listId, name, text, senderName, sendDate, status, campaignId })
         .then((data) => {
-          this.listId = null;
+          this.listId = '';
           this.name = '';
           this.text = '';
-          this.to = '';
           this.status = false;
           if (this.complete) {
             this.complete(data);
@@ -204,7 +200,7 @@ export default {
           this.drafts.forEach(draft => {
             if (draft.id == query.campaign_id) {
               vm.senderName = draft.sender_name;
-              vm.to = draft.list_id;
+              vm.listId = draft.list_id;
               vm.text = draft.text;
             }
           });
@@ -213,7 +209,7 @@ export default {
 
       // !isset(query[campaign_id])
       if (!('campaign_id' in this.$route.query)) {
-        this.senderName = this.to = this.text = '';
+        this.senderName = this.listId = this.text = '';
       }
     }
   }
