5b9ad9fd0aa6f8f1b7628a27b9d2616be0e2817f
smspartner2019 <devke03@gmail.com>
2019-04-18 08:58:11 +0300

    [footer] only leave the copyright

--- src/components/layouts/partials/Footer.vue
+++ src/components/layouts/partials/Footer.vue
@@ -1,27 +1,11 @@
 <template>
   <footer class="py-5" id="footer-main">
     <div class="container">
-      <div class="row align-items-center justify-content-xl-between">
-        <div class="col-xl-6">
-          <div class="copyright text-center text-xl-left text-muted">
-            &copy; 2018 <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+      <div class="row">
+        <div class="col">
+          <div class="copyright text-muted">
+            &copy; {{ year }} My SMS
           </div>
-        </div>
-        <div class="col-xl-6">
-          <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
-            </li>
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com/presentation" class="nav-link" target="_blank">About Us</a>
-            </li>
-            <li class="nav-item">
-              <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a href="https://www.creative-tim.com/license" class="nav-link" target="_blank">License</a>
-            </li>
-          </ul>
         </div>
       </div>
     </div>
@@ -30,6 +14,10 @@
 
 <script>
 export default {
-  
+  computed: {
+    year () {
+      return new Date().getFullYear();
+    }
+  }
 }
 </script>
