3aa452df84989907fab3d75c7fc95e169d7cb73a
smspartner2019 <devke03@gmail.com>
2019-04-24 10:09:52 +0300

    API sms deleted & assistance => mailto

--- src/components/layouts/partials/dashboard/Links.vue
+++ src/components/layouts/partials/dashboard/Links.vue
@@ -1,21 +1,24 @@
 <template>
   <ul v-if="user" class="navbar-nav align-items-center ml-md-auto">
     <li class="nav-item">
-      <a href="#" class="nav-link"><i class="fas fa-code"></i> API SMS</a>
+      <a :href="`mailto:${mailTo}`" class="nav-link"><i class="fas fa-question-circle"></i> Assistance</a>
     </li>
-    <li class="nav-item">
-      <a href="#" class="nav-link"><i class="fas fa-question-circle"></i> Assistance</a>
-    </li>
-    <li v-if="user.credits" class="nav-item">
-      <a href="#" class="nav-link">Balance <span class="badge badge-danger">{{ user.credits | formatCurrency }}</span></a>
+    <li v-if="user.credits" class="nav-link d-inline-flex align-items-center">
+      Credits <span class="ml-2 badge badge-danger">{{ user.credits | formatCurrency }}</span>
     </li>
   </ul>
 </template>
 <script>
+import { MAIL_TO } from '@/utils';
 export default {
   props: {
     user: {
       type: Object
+    }
+  },
+  computed: {
+    mailTo () {
+      return MAIL_TO;
     }
   }
 }
