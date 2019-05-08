22fb612b5d7f5707d597c5eb8b5baa3a123fda92
smspartner2019 <devke03@gmail.com>
2019-05-08 17:04:10 +0300

    display number of SMS

--- src/components/layouts/partials/dashboard/Links.vue
+++ src/components/layouts/partials/dashboard/Links.vue
@@ -7,7 +7,7 @@
       <a :href="`mailto:${mailTo}`" class="nav-link"><i class="fas fa-question-circle"></i> Assistance</a>
     </li>
     <li v-if="user !== null && user.credits" class="nav-link d-inline-flex align-items-center">
-      Crédits <span class="ml-2 badge badge-secondary">{{ user.credits | formatCurrency(2) }}</span>
+      Crédits <span class="ml-2 badge badge-secondary">{{ user.credits | formatCurrency(2) }} / {{ numberOfSMS | formatNumber }} SMS</span>
     </li>
   </ul>
 </template>
@@ -17,6 +17,7 @@ import { MAIL_TO } from '@/utils';
 
 export default {
   props: {
+    plans: Array,
     user: {
       type: Object
     }
@@ -25,11 +26,24 @@ export default {
   computed: {
     mailTo() {
       return MAIL_TO;
+    },
+    numberOfSMS() {
+      const plan = this.getPlan();
+      const { credits } = this.user;
+      let smsPrice;
+      let numberOfSMS;
+      if (!plan.length || !credits) return '0';
+      smsPrice = plan[0].smsPrice / 1000;
+      numberOfSMS = Math.floor(credits / smsPrice);
+      return numberOfSMS;
     }
   },
   methods: {
     toggleSidenav() {
       document.body.classList.toggle('g-sidenav-pinned');
+    },
+    getPlan() {
+      return this.plans.filter(({ id }) => id === this.user.planId);
     }
   }
 };
