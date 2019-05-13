0b53e4610b951254123cc620854657423ce92c8b
smspartner2019 <devke03@gmail.com>
2019-05-13 16:07:41 +0300

    fixes

--- src/filters.js
+++ src/filters.js
@@ -23,9 +23,7 @@ Vue.filter('capitalize', str => str.charAt(0).toUpperCase() + str.slice(1));
 Vue.filter('upper', str => str.toUpperCase());
 Vue.filter('lower', str => str.toLowerCase());
 Vue.filter('limit', (str, len, more = '...') => {
-  if (str.length <= len) {
-    return str;
-  }
+  if (str.length <= len) return str;
   return str.substring(0, len) + more;
 });
 Vue.filter('fullname', ({ firstname, lastname }) => `${lastname} ${firstname}`);
