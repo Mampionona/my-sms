8b2136a38aa971b6e753c683902942f006bbd6e1
Buzut <blog@buzut.fr>
2019-05-16 18:03:38 +0200

    🚑 register worker the proper way + small syntax changes

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import Worker from 'worker-loader?name=hash.worker.js!./worker'; // eslint-disable-line
+import Worker from './csv.worker';
 
 export const BAD_REQUEST = 400;
 export const UNAUTHENTICATED = 401;
